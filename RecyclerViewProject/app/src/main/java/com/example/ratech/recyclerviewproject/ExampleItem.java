9bf7abaeb2b044a5a2fca98f30b16715c2a14110
Robert Abo <robert.abo@hotmail.com>
2018-08-13 13:19:47 +0200

    RecyclerView: adding the ViewHolder and the Adapter and their logics (part 2)

---
+++ RecyclerViewProject/app/src/main/java/com/example/ratech/recyclerviewproject/ExampleItem.java
@@ -0,0 +1,27 @@
+package com.example.ratech.recyclerviewproject;
+
+public class ExampleItem {
+
+    private int mImageRessource;
+    private String mText1;
+    private String mText2;
+
+    public ExampleItem(int imageRessource, String text1, String text2) {
+        mImageRessource = imageRessource;
+        mText1 = text1;
+        mText2 = text2;
+    }
+
+    public int getImageRessource() {
+        return mImageRessource;
+    }
+
+    public String getText1() {
+        return mText1;
+    }
+
+    public String getText2() {
+        return mText2;
+    }
+
+}
